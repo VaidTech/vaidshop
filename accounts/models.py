7f5007bdc9a5885debecdbf3f6979e8a1481e9d3
mostafijru-rahman299 <mostafijur-rahman299@gmail.com>
2020-03-08 19:53:59 +0600

    update user_model

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 	is_owner	= models.BooleanField(default=False, null=True)
 	is_employee = models.BooleanField(default=False, null=True)
+
+	class Meta: 
+		managed = False # Ensure Django doesn't "manage" the table 
+		db_table = 'users' # Point to actual DB table
