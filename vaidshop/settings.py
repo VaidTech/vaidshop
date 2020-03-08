52281dad69eb6c4a529e2c7a00f49aaf29bb7b55
mostafijru-rahman299 <mostafijur-rahman299@gmail.com>
2020-03-08 21:02:45 +0600

    upd

--- vaidshop/settings.py
+++ vaidshop/settings.py
@@ -97,10 +97,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
