56d59fe2d34730cb3f1ee15f6ae16e6db718c539
mostafijru-rahman299 <mostafijur-rahman299@gmail.com>
2020-03-09 17:37:40 +0600

    migrations update

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,3 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 	is_owner	= models.BooleanField(blank=True, null=True)
 	is_employee = models.BooleanField(blank=True, null=True)
-
-	class Meta:
-		db_table = 'accounts_user'
