2c34b5aa2962ff862dceec029d5eac718a8c326c
mostafijru-rahman299 <mostafijur-rahman299@gmail.com>
2020-03-09 16:02:02 +0600

    error fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-class UserAdmin(admin.ModelAdmin):
-	list_display = ('id', 'username', 'is_owner')
-	list_display_links = ('id', 'username')
-	list_filter = ('is_owner', 'is_active')
-	search_fields = ('email', 'username', 'id')
-	
-admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
-	is_owner	= models.BooleanField(blank=True, null=True)
-	is_employee = models.BooleanField(blank=True, null=True)
+# class User(AbstractUser):
+# 	is_owner	= models.BooleanField(blank=True, null=True)
+# 	is_employee = models.BooleanField(blank=True, null=True)
 
-	class Meta:
-		db_table = 'accounts_user'
+# 	class Meta:
+# 		db_table = 'accounts_user'
