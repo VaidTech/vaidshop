dfd908595dabc1f1db06cd9dce258e8d65f87478
mostafijru-rahman299 <mostafijur-rahman299@gmail.com>
2020-03-09 11:34:10 +0600

    update db

--- vaidshop/settings.py
+++ vaidshop/settings.py
@@ -93,14 +93,14 @@ WSGI_APPLICATION = 'vaidshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'dbg'),
     }
 }
 
 
-# import dj_database_url 
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
